fe30f74e94d7c65077b2053c81e32a2fefe95208
Sagar Kanojia <isagarkanojia@gmail.com>
2022-02-18 23:54:15 +0530

    add: send message, get list of waves

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -8,13 +8,30 @@ contract WavePortal {
 
     uint256 totalWaves;
 
-    constructor() {
-        console.log("Yo yo, I am a contract by Sagar Kanojia");
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave {
+        address waver; // The address of the user who waved.
+        string message; // The message the user sent.
+        uint256 timestamp; // The timestamp when the user waved.
     }
 
-    function wave() public {
+    Wave[] waves;
+
+    constructor() {
+        console.log("Yo Yo, I'm a Smart Contract!");
+    }
+
+    function wave(string memory _message) public {
         totalWaves += 1;
-        console.log("%s has waved!",msg.sender);
+    
+        console.log("%s waved with meesage %s",msg.sender,_message);
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
--- scripts/run.js
+++ scripts/run.js
@@ -1,39 +1,38 @@
 const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
 
-    const [owner, randomPerson] = await hre.ethers.getSigners();
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
 
-    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
-    await waveContract.deployed();
+  console.log("Contract deployed to:", waveContract.address);
+  console.log("Contract deployed by:", owner.address);
 
-    console.log("Contract deployed to:", waveContract.address);
-    console.log("Contract deployed by:", owner.address);
+  let waveCount;
 
-    let waveCount;
+  waveCount = await waveContract.getTotalWaves();
 
-    waveCount = await waveContract.getTotalWaves();
+  let waveTxn = await waveContract.wave("From Youhuuuu!");
+  await waveTxn.wait();
 
-    let waveTxn = await waveContract.wave();
-    await waveTxn.wait();
+  waveCount = await waveContract.getTotalWaves();
 
-    waveCount = await waveContract.getTotalWaves();
+  waveTxn = await waveContract.connect(randomPerson).wave("Hellloooo Sagar!");
+  await waveTxn.wait();
 
-    waveTxn = await waveContract.connect(randomPerson).wave();
-    await waveTxn.wait();
-  
-    waveCount = await waveContract.getTotalWaves();
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
+};
 
-  };
-  
-  const runMain = async () => {
-    try {
-      await main();
-      process.exit(0); // exit Node process without error
-    } catch (error) {
-      console.log(error);
-      process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception' error
-    }
-    // Read more about Node exit ('process.exit(num)') status codes here: https://stackoverflow.com/a/47163396/7974948
-  };
-  
-  runMain();
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0); // exit Node process without error
+  } catch (error) {
+    console.log(error);
+    process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception' error
+  }
+  // Read more about Node exit ('process.exit(num)') status codes here: https://stackoverflow.com/a/47163396/7974948
+};
+
+runMain();
